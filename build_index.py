0039a7a88782952726121f764fe57e681e53672e
keysoze <mihai@Mihais-MacBook-Pro.local>
2018-06-19 13:01:29 -0400

    better index without stop words

--- build_index.py
+++ build_index.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 from ruamel.yaml import YAML
 yaml = YAML(typ='safe')
 
+stop_words = set([ "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "could", "did", "do", "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has", "have", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "it", "it's", "its", "itself", "let's", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "she", "she'd", "she'll", "she's", "should", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "we", "we'd", "we'll", "we're", "we've", "were", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "would", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves" ])
 
 from HTMLParser import HTMLParser
 # create a subclass and override the handler methods
@@ -53,19 +54,21 @@ for doc in docs.keys():
     with open(os.path.join(DOCS_DIR, item['p']), 'r') as help_file:
         html_content = help_file.read().strip()
     parser.feed(html_content)
-    title_content = re.sub('[^a-z]+', ' ', item['n'].lower()).strip()
-    text_content = ' ' + title_content + ' ' + parser.extract()
+    text_content = parser.extract()
     
     if text_content:
+        title_content = re.sub('[^a-z]+', ' ', item['n'].lower()).strip()
+        text_content += ' ' + title_content
         i = len(items)
 
         for token in text_content.split(' '):
-            tokens[token].add(i)
+            if token and not token in stop_words:
+                tokens[token].add(i)
         item['s'] = len(text_content)     
         items.append(item)     
 
 relevant_tokens = {}
-MAX = 50
+MAX = 70
 for token in tokens:
     idocs = tokens[token]
     if len(idocs) <= MAX:
